bc4b4175e2b5fd32ef317b4be8d75bc30078f798
chalund <114383740+chalund@users.noreply.github.com>
2024-05-27 08:08:03 +0200

    install

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Router, Routes, Route} from "react-router-dom"
+
+function App() {
+
+
+  return (
+   <Router>
+    <Routes>
+      <Route path="/" >
+        
+      </Route>
+    </Routes>
+   </Router>
+  )
+}
+
+export default App
